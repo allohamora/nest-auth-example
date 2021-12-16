4a8b95ea00c00aedb2ad49ae45e04278d403a029
Allohamora <herman2000@yandex.ru>
2021-12-16 21:48:20 +0200

    test: add tests for crypto.service

--- src/crypto/crypto.service.spec.ts
+++ src/crypto/crypto.service.spec.ts
@@ -12,7 +12,20 @@ describe('CryptoService', () => {
     service = module.get<CryptoService>(CryptoService);
   });
 
-  it('should be defined', () => {
+  test('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  test('scryptHash should return hashed string', async () => {
+    expect(typeof (await service.scryptHash('test'))).toBe('string');
+  });
+
+  test('scryptVerify should verify non hashed string with hashed', async () => {
+    const input = 'test';
+    const hashed = await service.scryptHash(input);
+
+    expect(await service.scryptVerify(input, hashed)).toBe(true);
+    expect(await service.scryptVerify('123', hashed)).toBe(false);
+    expect(await service.scryptVerify('123', '123:123')).toBe(false);
   });
 });
