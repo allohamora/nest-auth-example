a41edb3189c7ade8ad462119ae57bd697fd31df5
Allohamora <herman2000@yandex.ru>
2021-12-16 22:24:42 +0200

    refactor: remove unused imports and vars

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CryptoService } from 'src/crypto/crypto.service';
 import { Repository } from 'typeorm';
