e7d1d48a69a64c2249de2c83d2e26bd990170d8d
Allohamora <herman2000@yandex.ru>
2021-12-18 12:30:57 +0200

    feat: add swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
+import pkg from '../package.json';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './utils/entity-not-found-exception.filter';
+
+const SWAGGER_PATH = 'api';
+const SERVER_PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setTitle(pkg.name)
+    .setDescription(pkg.description)
+    .setVersion(pkg.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(SWAGGER_PATH, app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
