f5ae07f674134087b526f0b5429e6342f663496e
Allohamora <herman2000@yandex.ru>
2021-12-18 14:09:13 +0200

    fix: add missed ClassSerializerInterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import pkg from '../package.json';
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './utils/entity-not-found-exception.filter';
@@ -21,6 +21,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(SWAGGER_PATH, app, document);
 
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
 
